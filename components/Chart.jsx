0210e7093c25655a713a0ca24ff617df576de4e7
Darshil <vadherdarshil007@gmail.com>
2023-04-17 13:02:27 +0530

    dynamically chart created

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -1,66 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Mycomp = () => {
-  const [chartData, setChartData] = useState()
-  console.log(chartData)
-  useEffect(() => {
-    const data = [
-      {
-        name: 'Chintan',
-        maths: 95,
-        science: 84,
-        English: 74,
-        Physycs: 98,
-        Coding: 78,
-      },
-      {
-        name: 'Jaydeep',
-        maths: 93,
-        science: 34,
-        English: 74,
-        Physycs: 88,
-        Coding: 18,
-      },
-      {
-        name: 'Dhaval',
-        maths: 45,
-        science: 54,
-        English: 76,
-        Physycs: 78,
-        Coding: 88,
-      },
-      {
-        name: 'Aakash',
-        maths: 93,
-        science: 54,
-        English: 64,
-        Physycs: 48,
-        Coding: 79,
-      },
-    ];
-    setChartData(data)
-  }, [])
+  const data = [
+    {
+      name: 'Chintan',
+      maths: 95,
+      science: 84,
+      English: 74,
+      Physycs: 98,
+      Coding: 78,
+    },
+    {
+      name: 'Jaydeep',
+      maths: 93,
+      science: 34,
+      English: 74,
+      Physycs: 88,
+      Coding: 18,
+    },
+    {
+      name: 'Dhaval',
+      maths: 45,
+      science: 54,
+      English: 76,
+      Physycs: 78,
+      Coding: 88,
+    },
+    {
+      name: 'Aakash',
+      maths: 93,
+      science: 54,
+      English: 64,
+      Physycs: 48,
+      Coding: 79,
+    },
+  ];
+
+  let news = ["#8df552", "#6665ff", "#27ef99", "#ff0000d4", "#ffe72d",]
+
+  const keys = Object.keys(data[0]).filter(key => key !== "name" && key !== "fill");
+
+  const bars = keys.map((key, index) => (
+    <Bar key={key} dataKey={key} fill={news[index % news.length]} />
+  ));
+
+
+
 
   return (
     <div className='flex justify-center items-center h-98'>
-      <LineChart width={600} height={300} data={chartData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="maths" stroke="#8df552" />
-        <Line type="monotone" dataKey="Coding" stroke="#6665ff" />
-        <Line type="monotone" dataKey="science" stroke="#27ef99" />
-        <Line type="monotone" dataKey="Physycs" stroke="#ff0000d4" />
-        <Line type="monotone" dataKey="English" stroke="#ffe72d" />
-      </LineChart>
+      <ResponsiveContainer width="50%" height={500}>
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {bars}
+        </BarChart>
+      </ResponsiveContainer>
     </div>
 
   )
 }
 
 export default Mycomp
-
-let news = ["#8df552", "#6665ff", "#27ef99", "#ff0000d4", "#ffe72d",]
